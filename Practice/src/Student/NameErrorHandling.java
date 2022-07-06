a9b534280b3d99f37332b7fb004ba34a57dd943a
Conred Alas-as <conredalasas12@gmail.com>
2022-07-06 22:40:40 +0800

    Basic java CRUD no Database just run in terminal

---
+++ Practice/src/Student/NameErrorHandling.java
@@ -0,0 +1,23 @@
+package Student;
+
+public class NameErrorHandling extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	private String message;
+
+	public NameErrorHandling(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
